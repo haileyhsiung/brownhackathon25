02de557366a636b7a28407a940bcbcd3bfe498cb
Eric Zheng <112358392+ezheng34@users.noreply.github.com>
2025-02-01 22:40:29 -0500

    connect signup

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -11,9 +11,6 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
-/**
- * Render the App (mock) element to front-end using React
- */
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
